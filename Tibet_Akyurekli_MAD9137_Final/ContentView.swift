d8e3b5839d77cbdc8a9ef10d6825212daae5035d
Akyu0002 <akyu0002@algonquinlive.com>
2022-11-30 15:41:15 -0500

    Initial Commit

---
+++ Tibet_Akyurekli_MAD9137_Final/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Tibet_Akyurekli_MAD9137_Final
+//
+//  Created by Tibet Akyurekli on 2022-11-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tibet_Akyurekli_MAD9137_Final/Tibet_Akyurekli_MAD9137_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tibet_Akyurekli_MAD9137_FinalApp.swift
+//  Tibet_Akyurekli_MAD9137_Final
+//
+//  Created by Tibet Akyurekli on 2022-11-30.
+//
+
+import SwiftUI
+
+@main
+struct Tibet_Akyurekli_MAD9137_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
